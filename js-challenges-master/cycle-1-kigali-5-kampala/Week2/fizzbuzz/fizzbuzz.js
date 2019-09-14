9b9c05977f6508d915cbd7f016478ea18f684a55
Kanyunyuzi Cleopatra <kanyunyuzicleo@gmail.com>
2019-09-14 09:44:24 -0700

    Update fizzbuzz.js

--- js-challenges-master/cycle-1-kigali-5-kampala/Week2/fizzbuzz/fizzbuzz.js
+++ js-challenges-master/cycle-1-kigali-5-kampala/Week2/fizzbuzz/fizzbuzz.js
@@ -2,21 +2,24 @@
     // Your code here
 
     var combinedlength= list1.concat(list2);
+   
+    var res = "";
     
     var x  = combinedlength.length;
-    
-            if(x% 3===0 && x%5===0){
-console.log("fizzbuzz");
+   
+   
+if(x% 3==0 && x%5==0){res += "Fizzbuzz";}
 
-        }
-else if (x%3 === 0){console.log("Fizz");
+        
+else if (x%3 == 0){res +="Fizz";}
+
+else if (x%5 == 0) {res +="Buzz";}
+       
+
+else 
+{res = x ;}
+          
+return res;
 }
-else if (x%5 === 0) {console.log("Buzz");
-       }
-else {console.log()}
-
-   }
-
-
 
 module.exports = fizzBuzz;
